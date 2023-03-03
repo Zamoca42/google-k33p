24797474e8eea44529e9c6140aca6fdce53013e0
Zamoca <suntail2002@naver.com>
2023-03-03 20:15:46 +0900

    Basic template engine

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -4,9 +4,16 @@ from flask import render_template
 def create_app():
     print('run: create_app()')
     app = Flask(__name__)
+
+    if app.config['DEBUG']:
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
     
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.errorhandler(404)
+    def page_404(error):
+        return render_template('/404.html'), 404
     
     return app
